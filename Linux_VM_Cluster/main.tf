d91426c028616fe102da290fef5354c3dae15dc0
Ruwinda Fernando <ruwindada@gmail.com>
2025-02-08 22:53:11 +0100

    Commented Loadbalancer

--- Linux_VM_Cluster/main.tf
+++ Linux_VM_Cluster/main.tf
@@ -29,29 +29,29 @@ resource "azurerm_subnet" "test" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_public_ip" "test" {
-  name                = "publicIPForLB"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-}
+# resource "azurerm_public_ip" "test" {
+#   name                = "publicIPForLB"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   allocation_method   = "Static"
+# }
 
-resource "azurerm_lb" "test" {
-  name                = "loadBalancer"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_lb" "test" {
+#   name                = "loadBalancer"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  frontend_ip_configuration {
-    name                 = "internalLoadBalancer"
-    subnet_id = azurerm_subnet.test.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#   frontend_ip_configuration {
+#     name                 = "internalLoadBalancer"
+#     subnet_id = azurerm_subnet.test.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
-resource "azurerm_lb_backend_address_pool" "test" {
-  loadbalancer_id = azurerm_lb.test.id
-  name            = "BackEndAddressPool"
-}
+# resource "azurerm_lb_backend_address_pool" "test" {
+#   loadbalancer_id = azurerm_lb.test.id
+#   name            = "BackEndAddressPool"
+# }
 
 resource "azurerm_network_interface" "test" {
   count               = 2
