f023d57d2dbae9d559ac826413456aafd01f643f
Ruwinda Fernando <ruwindada@gmail.com>
2025-02-16 00:06:15 +0100

    Removed duplicate output definition

--- Linux_VM_Cluster/ssh.tf
+++ Linux_VM_Cluster/ssh.tf
@@ -17,8 +17,3 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 output "key_data" {
   value = azapi_resource_action.ssh_public_key_gen.output.publicKey
 }
-
-output "private_key" {
-  value     = azapi_resource_action.ssh_public_key_gen.output.privateKey
-  sensitive = true
-}
