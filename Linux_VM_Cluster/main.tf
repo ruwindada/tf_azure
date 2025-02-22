87c6e9b88230d4298eab6cef205a1cebacb29d51
Ruwinda Fernando <ruwindada@gmail.com>
2025-02-22 23:32:09 +0100

    Updated network security group (ssh)

--- Linux_VM_Cluster/main.tf
+++ Linux_VM_Cluster/main.tf
@@ -17,6 +17,29 @@ resource "azurerm_subnet" "test" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.test.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_public_ip" "test" {
   name                = "publicIPForLB"
   location            = azurerm_resource_group.rg.location
