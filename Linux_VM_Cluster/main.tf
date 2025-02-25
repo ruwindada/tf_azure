6f50a975a53b31fdb1cb6f84bb959a0230d6c794
Ruwinda Fernando <ruwindada@gmail.com>
2025-02-25 23:19:27 +0100

    Updated custom script, removed hostname

--- Linux_VM_Cluster/main.tf
+++ Linux_VM_Cluster/main.tf
@@ -213,11 +213,8 @@ resource "azurerm_linux_virtual_machine" "test" {
     sudo systemctl enable sshd
     sudo systemctl start sshd
 
-    # Get the hostname
-    HOSTNAME=$(hostname)
-
     # Create a simple HTML file
-    echo '<html><body><h1>Welcome to Rocky Linux 9.4</h1><p>Hostname: $${HOSTNAME}</p></body></html>' | sudo tee /var/www/html/index.html
+    echo '<html><body><h1>Welcome to Rocky Linux 9.4</h1></body></html>' | sudo tee /var/www/html/index.html
     EOT
   )
 }
