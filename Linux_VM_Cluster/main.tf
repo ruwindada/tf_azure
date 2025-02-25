a9b941211759731da7a7258a3f2e6bbb2e3ac272
Ruwinda Fernando <ruwindada@gmail.com>
2025-02-25 21:49:45 +0100

    Updated security rule for https

--- Linux_VM_Cluster/main.tf
+++ Linux_VM_Cluster/main.tf
@@ -45,6 +45,17 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
   security_rule {
+    name                       = "allow_http"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
     name                       = "allow_lb"
     priority                   = 1003
     direction                  = "Inbound"
