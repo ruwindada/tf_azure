3f62cbbc066b8cfb29e166b19eee086d815b4da7
Ruwinda Fernando <ruwindada@gmail.com>
2025-03-01 23:34:32 +0100

    Updated frontend_ip_configuration

--- Linux_VM_Cluster/main.tf
+++ Linux_VM_Cluster/main.tf
@@ -88,7 +88,6 @@ resource "azurerm_lb" "test" {
   frontend_ip_configuration {
     name                 = "internalLoadBalancer"
     #public_ip_address_id = azurerm_public_ip.test.id
-    #public_ip_address_id = azurerm_public_ip.test.id
     subnet_id = azurerm_subnet.test.id
     private_ip_address_allocation = "Dynamic"
   }
